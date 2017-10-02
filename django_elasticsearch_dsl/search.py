6de003f0133fedcc778f44470620cee13bf91e26
sabricot <julien.sabre@epitech.eu>
2017-10-02 22:44:04 +0200

    WIP Add a way to retrieve a django queryset

---
+++ django_elasticsearch_dsl/search.py
@@ -0,0 +1,33 @@
+from django.db.models import Case, When
+
+from elasticsearch_dsl import Search as DSLSearch
+
+
+class Search(DSLSearch):
+    def __init__(self, **kwargs):
+        self._model = kwargs.pop('model', None)
+        super(Search, self).__init__(**kwargs)
+
+    def _clone(self):
+        s = super(Search, self)._clone()
+        s._model = self._model
+        return s
+
+    def to_queryset(self, keep_order=True):
+        """
+        This method return a django queryset from the an elasticsearch result.
+        It cost a query to the sql db.
+        """
+        s = self._clone()
+        s = s.source(False)
+        pks = [result._id for result in s]
+
+        qs = self._model.objects.filter(pk__in=pks)
+
+        if keep_order:
+            preserved_order = Case(
+                *[When(pk=pk, then=pos) for pos, pk in enumerate(pks)]
+            )
+            qs = qs.order_by(preserved_order)
+
+        return qs
